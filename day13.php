9591419f45768c561fa6035bc345b0ddf87f77a2
cnelson20 <cjnelson2004@gmail.com>
2020-12-14 15:42:12 -0500

    Will give the correct answer during the heat death of the universe

--- day13.php
+++ day13.php
@@ -29,5 +29,30 @@ echo "\nPart 1: " . ($wait_time * $bus_no) . "\n";
 $p2_bus = array_diff(explode(",",str_replace("\n","",$text[1])),array("x"));
 foreach($p2_bus as &$val) {$val = intval($val);}
 
-var_dump($p2_bus);
+var_dump(array_keys($p2_bus));
+var_dump(array_values($p2_bus));
 //return 1;
+
+function gcd ($a, $b) {
+    return $b ? gcd($b, $a % $b) : $a;
+}
+function lcm_array($arr) {
+  $lcm = 1;
+  foreach ($arr as $val) {
+    $lcm = $lcm * $val / gcd($lcm,$val);
+  }
+  return $lcm;
+}
+
+$i = $filename == "day13.txt" ? 100000000000000 : 0;
+while ($i < 2 * lcm_array($p2_bus)) {
+$t = true;
+  echo $i . "\n";
+  foreach ($p2_bus as $key => $value) {
+    if (0 != ($i + $key) % $value) {$t = false; break;}
+  }
+  if ($t) {break;}
+  $i += array_values($p2_bus)[0];
+}
+echo "\n";
+echo lcm_array($p2_bus);
